cc494027763beb8b2ebfc96616b756a1f06e6377
Yulv-git <yulvchi@qq.com>
2022-04-11 01:39:55 +0800

    Optimize shell scripts.

--- search_typos.sh
+++ search_typos.sh
@@ -4,38 +4,48 @@
  # @Date: 2022-04-09 22:01:30
  # @Motto: Entities should not be multiplied unnecessarily.
  # @LastEditors: Shuangchi He
- # @LastEditTime: 2022-04-10 21:40:07
- # @FilePath: /Search-for-Typos-in-Code-or-Text/search_typos.sh
+ # @LastEditTime: 2022-04-11 01:38:03
+ # @FilePath: /Search-for-Typos/search_typos.sh
  # @Description: Search for typos in code or text.
 ### 
 
-grep -r -i -w -n "do do" ./
-grep -r -i -w -n "does does" ./
-grep -r -i -w -n "have have" ./
-grep -r -i -w -n "is is" ./
-grep -r -i -w -n "are are" ./
+target_dir='./'
 
-grep -r -i -w -n "of of" ./
-grep -r -i -w -n "for for" ./
-grep -r -i -w -n "in in" ./
-grep -r -i -w -n "on on" ./
-grep -r -i -w -n "at at" ./
-grep -r -i -w -n "to to" ./
-grep -r -i -w -n "with with" ./
-grep -r -i -w -n "from from" ./
+# -------------------------------------------------------------------------------------------
 
-grep -r -i -w -n "a a" ./
-grep -r -i -w -n "one one" ./
+repeated_words_en=("do" "does" "have" "is" "are" \  # 动词
+                  "of" "for" "in" "on" "at" "to" "with" "from" \  # 介词
+                  "a" "one"  # 冠词
+                  )
 
-grep -r -i -n "modle" ./  # model, module
-grep -r -i -n "achive" ./  # archive, achieve
-grep -r -i -n "attrbute" ./  # attribute
-grep -r -i -n "recieve" ./  # receive
-grep -r -i -n "indicies" ./  # indices
-grep -r -i -n "instrution" ./  # instruction
-grep -r -i -n "occurences" ./  # occurrences
-grep -r -i -n "atleast" ./  # at least
-grep -r -i -n "ouput" ./  # outputs
-grep -r -i -n "retrive" ./  # retrieve
+for item in ${repeated_words_en[@]}; do
+    item2="${item} ${item}"
+    echo
+    echo "Could the word '${item}' be repeated???"
+    grep -r -i -w -n "${item2}" ${target_dir}
+done
 
-grep -r -n "的的" ./
+# -------------------------------------------------------------------------------------------
+
+repeated_words_zh=("的" "是" "不" "就" \
+                  "有" "为"  # 可能有“没有有关、因为为了”的例外
+                  )  
+
+for item in ${repeated_words_zh[@]}; do
+    item2="${item}${item}"
+    echo
+    echo "Could the word '${item}' be repeated???"
+    grep -r -n "${item2}" ${target_dir}
+done
+
+# -------------------------------------------------------------------------------------------
+
+typos_en=("modle" \  # typos本身没有释义
+         "achive" "attrbute" "recieve" "indicies" "instrution" "occurences" "atleast" "ouput" "retrive"  # 存在网络释义，即不规范用法的
+         )
+
+for item in ${typos_en[@]}; do
+    echo
+    echo "Could the word / string '${item}' be a typo???"
+    grep -r -i -n "${item}" ${target_dir}
+done
