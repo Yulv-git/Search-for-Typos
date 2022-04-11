242f7d609be29a1fa654277e2e42e31cecc0661b
Yulv-git <yulvchi@qq.com>
2022-04-11 11:55:59 +0800

    Update shell.

--- search_typos.sh
+++ search_typos.sh
@@ -4,19 +4,20 @@
  # @Date: 2022-04-09 22:01:30
  # @Motto: Entities should not be multiplied unnecessarily.
  # @LastEditors: Shuangchi He
- # @LastEditTime: 2022-04-11 10:42:16
+ # @LastEditTime: 2022-04-11 11:35:09
  # @FilePath: /Search-for-Typos/search_typos.sh
  # @Description: Search for typos in code or text.
+ # Repository: https://github.com/Yulv-git/Search-for-Typos
 ### 
 
 target_dir="$1"
 echo "target_dir: ${target_dir}"
 
-# -------------------------------------------------------------------------------------------
 
 repeated_words_en=("do" "does" "have" "is" "are" \
                   "of" "for" "in" "on" "at" "to" "with" "from" \
                   "a" "an" "the" \
+                  "this" \
                   "one"
                   )
 
@@ -27,20 +28,18 @@ for item in ${repeated_words_en[@]}; do
     grep -r -i -w -n --color=auto "${item2}" ${target_dir}
 done
 
-# -------------------------------------------------------------------------------------------
 
 repeated_words_zh=("的" "是" "不" "就" "我" "他" \
-                  "有" "为"
+                  "为"
                   )  
 
 for item in ${repeated_words_zh[@]}; do
     item2="${item}${item}"
     echo
-    echo "Could the word '${item}' be repeated???"
+    echo "Could the character '${item}' be repeated???"
     grep -r -n --color=auto "${item2}" ${target_dir}
 done
 
-# -------------------------------------------------------------------------------------------
 
 typos_en=("modle" \
          "achive" "attrbute" "recieve" "indicies" "instrution" "occurences" "atleast" "ouput" "retrive"
@@ -48,6 +47,6 @@ typos_en=("modle" \
 
 for item in ${typos_en[@]}; do
     echo
-    echo "Could the word / string '${item}' be a typo???"
+    echo "Could the word/string ${item} be a typo???"
     grep -r -i -n --color=auto "${item}" ${target_dir}
 done
