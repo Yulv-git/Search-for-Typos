bab9a9230f05b9f55674cbecd30f3b288d86b421
Yulv-git <yulvchi@qq.com>
2022-06-03 14:07:20 +0800

    Update auto_fix.

--- auto_fix.sh
+++ auto_fix.sh
@@ -4,9 +4,11 @@
  # @Date: 2022-06-02 01:35:15
  # @Motto: Entities should not be multiplied unnecessarily.
  # @LastEditors: Shuangchi He
- # @LastEditTime: 2022-06-02 20:40:55
+ # @LastEditTime: 2022-06-03 14:06:08
  # @FilePath: /Search-for-Typos/auto_fix.sh
- # @Description: Modify here please
+ # @Disclaimer: Use the auto_fix.sh script only when you are very sure that the typo(s) and
+ #              target paths you want to modify are accurate and correct. Because batch automatic
+ #              modification brings convenience, it may also bring wrong modification.
 ### 
 
 current_dir=$(cd `dirname $0`; pwd)
@@ -17,9 +19,17 @@ echo "target_dir: ${target_dir}"
 # auto fix some typos
 while read item
 do
-    item2=(${item//'-->'/' '})
+    item2=(${item//"-->"/" "})
     item_key=${item2[0]}
     item_value=${item2[1]}
+
+    if [[ ${item_key} =~ "Space=Blank" ]]; then
+        item_key=${item_key//"Space=Blank"/" "}
+    fi
+    if [[ ${item_value} =~ "Space=Blank" ]]; then
+        item_value=${item_value//"Space=Blank"/" "}
+    fi
+
     echo "${item_key} --> ${item_value}"
-    sed -i "s/${item_key}/${item_value}/g" `grep ${item_key} -rl ${target_dir} --exclude-dir=.git`
+    sed -i "s/${item_key}/${item_value}/g" `egrep "${item_key}" -rl ${target_dir} --exclude-dir=.git`
 done < ${current_dir}/typos_lib/auto_fix.txt
